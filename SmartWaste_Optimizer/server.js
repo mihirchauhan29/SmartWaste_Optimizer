b68cf56a60ca78319049038cef1bbf4567200b0b
mihirchauhan29 <cmihir044@gmail.com>
2024-09-04 03:29:58 +0530

    Set up basic server and database connection

--- SmartWaste_Optimizer/server.js
+++ SmartWaste_Optimizer/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { Pool } = require('pg');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// PostgreSQL connection setup
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database', err.stack);
+    } else {
+        console.log('Connected to the database');
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Urban Waste Management Optimization API');
